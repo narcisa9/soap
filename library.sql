482154579d3df27e1706bc43d23c1933f1a63e7a
narcisa9 <narcisaangheloiu@gmail.com>
2018-01-22 07:44:23 +0200

    update  database

--- library.sql
+++ library.sql
@@ -21,9 +21,8 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
-
 --
--- Table structure for table `autori`
+-- Structura de tabel pentru tabelul `autori`
 --
 
 CREATE TABLE `autori` (
@@ -33,46 +32,45 @@ CREATE TABLE `autori` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `autori`
+-- Salvarea datelor din tabel `autori`
 --
 
 INSERT INTO `autori` (`id_autor`, `nume_autor`, `status`) VALUES
-(1, 'autor1', 0),
-(2, 'autor2', 0),
-(3, 'autor1', 1),
-(4, 'autor2', 1),
-(5, 'autor3e', 0),
-(6, 'sas3', 0),
-(7, '', 0),
-(8, 'autor', 0),
-(9, 'autor3', 1);
+(1, 'autor1', 1),
+(2, 'autor2', 1),
+(3, 'autor3', 1),
+(10, 'autor4', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `carti`
+-- Structura de tabel pentru tabelul `carti`
 --
 
 CREATE TABLE `carti` (
   `id_carte` int(11) NOT NULL,
   `titlu_carte` varchar(60) NOT NULL,
   `id_autor` varchar(60) NOT NULL,
-  `status` tinyint(1) NOT NULL
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `booking` tinyint(4) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `carti`
+-- Salvarea datelor din tabel `carti`
 --
 
-INSERT INTO `carti` (`id_carte`, `titlu_carte`, `id_autor`, `status`) VALUES
-(1, 'carte1', '3', 1),
-(2, 'carte2', '3,4', 1),
-(3, 'carte3', '3,4,9', 1);
+INSERT INTO `carti` (`id_carte`, `titlu_carte`, `id_autor`, `status`, `booking`) VALUES
+(0, 'carte1', '1,3,10', 1, 1),
+(2, 'carte2', '2,3', 1, 0),
+(3, 'carte3', '1,2', 1, 0),
+(7, 'carte4', '1,2,3', 1, 1),
+(10, 'carte5', '1,2', 1, 0),
+(13, 'carte6', '1,3,10', 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `clienti`
+-- Structura de tabel pentru tabelul `clienti`
 --
 
 CREATE TABLE `clienti` (
@@ -82,20 +80,19 @@ CREATE TABLE `clienti` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `clienti`
+-- Salvarea datelor din tabel `clienti`
 --
 
 INSERT INTO `clienti` (`id_client`, `nume_client`, `status`) VALUES
-(1, 'client1', 0),
-(2, 'client1', 1),
-(3, 'client3', 0),
-(4, 'client2', 1),
-(5, 'client3', 1);
+(1, 'client1', 1),
+(2, 'client2', 1),
+(5, 'client4', 1),
+(8, 'client5', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `imprumuturi`
+-- Structura de tabel pentru tabelul `imprumuturi`
 --
 
 CREATE TABLE `imprumuturi` (
@@ -106,6 +103,15 @@ CREATE TABLE `imprumuturi` (
   `numar_zile` int(11) NOT NULL,
   `status` tinyint(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Salvarea datelor din tabel `imprumuturi`
+--
+
+INSERT INTO `imprumuturi` (`id_imprumut`, `id_client`, `id_carte`, `data_imprumut`, `numar_zile`, `status`) VALUES
+(15, 1, 0, '2018-01-18', 2, 1),
+(11, 2, 7, '2018-01-19', 3, 1),
+(12, 8, 13, '2018-01-01', 2, 1);
 
 --
 -- Indexes for dumped tables
@@ -143,22 +149,27 @@ ALTER TABLE `imprumuturi`
 -- AUTO_INCREMENT for table `autori`
 --
 ALTER TABLE `autori`
-  MODIFY `id_autor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_autor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- AUTO_INCREMENT for table `carti`
 --
 ALTER TABLE `carti`
-  MODIFY `id_carte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_carte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
 --
 -- AUTO_INCREMENT for table `clienti`
 --
 ALTER TABLE `clienti`
-  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
 --
 -- AUTO_INCREMENT for table `imprumuturi`
 --
 ALTER TABLE `imprumuturi`
-  MODIFY `id_imprumut` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_imprumut` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
